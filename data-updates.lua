d1db8b69460448087c3e6543f021f1bc5f9b8571
Nihilistzsche <nihilistzsche@gmail.com>
2022-12-15 03:38:46 -0800

    Since these are identical we can just use or here.

--- data-updates.lua
+++ data-updates.lua
@@ -1,6 +1,3 @@
-if mods["Krastorio2"] then
-    data.raw.recipe["fish-farm"].ingredients={{"raw-fish", 20}, {"glass", 25}, {"iron-plate", 10}}
-end
-if mods["aai-industry"] and not mods["Krastorio2"] then
+if mods["Krastorio2"] or mods["aai-industry"] then
     data.raw.recipe["fish-farm"].ingredients={{"raw-fish", 20}, {"glass", 25}, {"iron-plate", 10}}
 end
